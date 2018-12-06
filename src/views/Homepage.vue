324444705efe0ac35c85a765312504cf8c16f48c
Astrid Holmberg <Astrid@student-227-93.eduroam.uu.se>
2018-12-06 14:29:36 +0100

    homepage

---
+++ src/views/Homepage.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="styling">
+Hej
+    <button v-on:click="switchLang()">{{ uiLabels.language }}</button>
+    <router-link to="ordering">Order stuff</router-link>
+</div>
+</template>
+<script>
+
+//import methods and data that are shared between ordering and kitchen views
+import sharedVueStuff from '@/components/sharedVueStuff.js'
+
+export default {
+  name: 'Homepage',
+  components: {
+
+  },
+  mixins: [sharedVueStuff], // include stuff that is used in both
+                            //the ordering system and the kitchen
+  data: function(){
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+
+
+  h1 {
+    text-transform: uppercase;
+    font-size: 1.4em;
+  }
+</style>
